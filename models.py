12a74e123d7e5cfa73a1c19bdd0757b67b1eb857
marvel17a <marvelstudio17a@gmail.com>
2025-12-06 07:56:39 +0530

    Update

--- models.py
+++ models.py
@@ -1,1 +1,28 @@
+# models.py
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from database import Base
+import datetime
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=False)
+    
+    # Relationship: One user has many products
+    products = relationship("Product", back_populates="owner")
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(Text)
+    image_url = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    # Relationship: Product belongs to one owner
+    owner = relationship("User", back_populates="products")
+  
